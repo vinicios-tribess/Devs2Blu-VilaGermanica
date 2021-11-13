c1a30b0213894ceeeb2740f23340ad3992d6aa3c
jucabnu <julianovieira.bc@gmail.com>
2021-11-12 21:39:29 -0300

    Mais ajustes no código e no visual

--- js/jogo.js
+++ js/jogo.js
@@ -9,74 +9,129 @@ var q1 = {pergunta : "Qual dessas medidas NÃO é cientificamente comprovada par
             correta : "Tratamento com cloroquina", 
             explicacao : "O tratamento com cloroquina não é uma medida cientificamente comprovada para o combate e prevenção da Covid-19, todas as demais estão corretas."};
 
-var q2 = {pergunta : "Segunda pergunta?", 
+var q2 = {pergunta : "Os sintomas mais comuns da COVID-19 são:", 
+            alternativa1 : "perda de visão, tontura e pele amarelada", 
+            alternativa2 : "febre, tosse, cansaço, dor de cabeça e alteração do olfato", 
+            alternativa3 : "queda de cabelo, coceira na pele e falta de apetite", 
+            alternativa4 : "pele avermelhada, catarro e vista embaçada", 
+            correta : "febre, tosse, cansaço, dor de cabeça e alteração do olfato", 
+            explicacao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper dictum sem, ac volutpat ipsum. Donec rutrum nibh interdum justo lacinia, vitae rutrum elit tempor."};
+
+var q3 = {pergunta : "Terceira pergunta?", 
             alternativa1 : "alternativa1", 
-            alternativa2 : "alternativa2", 
+            alternativa2 : "Correta", 
             alternativa3 : "alternativa3", 
             alternativa4 : "alternativa4", 
-            correta : "alternativa4", 
-            explicacao : "Explicação."};
+            correta : "Correta", 
+            explicacao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nunc erat, ultricies nec semper ac, euismod sed enim. Etiam vitae ultrices magna. Ut et faucibus neque."};            
 
-var perguntas = [q1, q2]; // array com 6 objetos contendo as que
+let perguntas = [q1, q2, q3]; // array com 6 objetos contendo as que
+let acertos = 0;
+let erros = 0;
+let questao = 0;
+let campoAcertos = 0;
+let campoErros = 0;
+
+let p = "";
+let a1 = "";
+let a2 = "";
+let a3 = "";
+let a4 = "";
+let resultado = "";
+let jogo = "";
+let lista = "";
 
 // coloca a questão na tela
 
-var jogo = document.querySelector(".jogo");
+function montaQuestao(){
 
-var p = jogo.querySelector("#pergunta");
-var a1 = jogo.querySelector("#alternativa-1");
-var a2 = jogo.querySelector("#alternativa-2");
-var a3 = jogo.querySelector("#alternativa-3");
-var a4 = jogo.querySelector("#alternativa-4");
-var resultado = jogo.querySelector("#resultado");
+    jogo = document.querySelector(".jogo");
 
-p.textContent = perguntas[0].pergunta;
-a1.textContent = perguntas[0].alternativa1;
-a2.textContent = perguntas[0].alternativa2;
-a3.textContent = perguntas[0].alternativa3;
-a4.textContent = perguntas[0].alternativa4;
-resultado.textContent = "Escolha uma das alternativas";
+    p = jogo.querySelector("#pergunta");
+    a1 = jogo.querySelector("#alternativa-1");
+    a2 = jogo.querySelector("#alternativa-2");
+    a3 = jogo.querySelector("#alternativa-3");
+    a4 = jogo.querySelector("#alternativa-4");
+    resultado = jogo.querySelector("#resultado");
+    campoAcertos = jogo.querySelector("#acertos");
+    campoErros = jogo.querySelector("#erros");
 
-console.log(a1);
+    p.textContent = perguntas[questao].pergunta;
+    a1.textContent = perguntas[questao].alternativa1;
+    a2.textContent = perguntas[questao].alternativa2;
+    a3.textContent = perguntas[questao].alternativa3;
+    a4.textContent = perguntas[questao].alternativa4;
+    resultado.textContent = "Escolha uma das alternativas";
+    
+    lista = document.querySelector("#lista-alternativas");
 
-// capturar a resposta
+}
 
-var r;
-a1.addEventListener("click", function(){    
-    r = a1.textContent;
-});
-
-/*
-a2.addEventListener("click", function(){
-    var resposta = a2.textContent;
-});
-a3.addEventListener("click", function(){
-    var resposta = a2.textContent;
-});
-a4.addEventListener("click", function(){
-    var resposta = a2.textContent;
-});
-*/
-console.log("Clicou em: " + r);
+montaQuestao();
 
 
 
+lista.addEventListener("click", function(event){
 
+    var escolha = event.target.parentNode.textContent;
+    var correta = perguntas[questao].correta;
+    var mensagem = "";
 
+    if(escolha == correta){
+        //event.target.parentNode.classList.add("correta");        
+        console.log("acertou!");
+        acertos = acertos + 1;        
+        mensagem = "Parabéns, você acertou: ";
+        
+    }else{
+        //event.target.parentNode.classList.add("errada");
+        console.log("Errou.")
+        erros = erros + 1;        
+        mensagem = "Infelizmente você errou: ";
+    }
+    
+    setTimeout(function(){},500);
 
-    // for para percorrer o array de perguntas
+    campoAcertos.textContent = acertos;
+    campoErros.textContent = erros;
+    resultado.textContent = perguntas[questao].explicacao;
 
-        // captura a resposta
+    console.log(escolha);    
+    questao = questao + 1;    
 
+})
 
-            // testa se acertou (comparação)
+let botao = document.querySelector("#proxima");
 
-            // mostra o hint
+botao.addEventListener("click",function(){
 
+    if(questao < perguntas.length){
+        montaQuestao();
+    }else{
+        fimDeJogo();
+    }
 
-// troca a questão
+})
 
+function fimDeJogo(){
 
+    let mensagemFim = "Você acertou " + acertos + " perguntas, ";
+
+    console.log(acertos);
+
+    switch(acertos){
+        case 0 : mensagemFim = mensagemFim + "então sabe pouco sobre a prevenção, Que tal dar uma olha neste site https://portal.fiocruz.br/coronavirus/perguntas-e-respostas ?";
+        break;
+        case 1 : mensagemFim = mensagemFim + "então está ficando bom, que tal ler esse artigo (link) para melhorar seu conhecimento sobre Covid-19?";
+        break;
+        case 2 : mensagemFim = mensagemFim + "então sabe bastante sobre prevenção, esperamos que as dicas durante o jogo, tenham melhorado ainda mais seu conhecimento sobre prevenção da Covid-19";
+        break;
+        case 3 : mensagemFim = mensagemFim + "parabéns, você está sabe como se prevenir, ajude a conscientizar outras pessoas.";
+        break;
+    }
+
+        resultado.textContent = mensagemFim;
+}
 
 
 
